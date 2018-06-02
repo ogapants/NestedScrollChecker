889c2b83db63e33ab98fbc8a54813d268cabcae1
ogapants <ogapamyu@gmail.com>
2018-06-02 23:24:35 +0900

    fix activity

--- app/src/main/java/com/github/ogapants/nestedscrollchecker/MainActivity.kt
+++ app/src/main/java/com/github/ogapants/nestedscrollchecker/MainActivity.kt
@@ -1,24 +1,49 @@
 package com.github.ogapants.nestedscrollchecker
 
 import android.os.Bundle
+import android.support.annotation.LayoutRes
+import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
-import android.widget.Toast
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.RadioGroup
 
 class MainActivity : AppCompatActivity() {
 
-    companion object {
-        var a = 0
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        if (a % 2 == 0) {
-            Toast.makeText(this, "--25--", Toast.LENGTH_SHORT).show()
-            setContentView(R.layout.activity_main25)
-        } else {
-            Toast.makeText(this, "--27--", Toast.LENGTH_SHORT).show()
-            setContentView(R.layout.activity_main27)
+        setContentView(R.layout.activity_main)
+        val radioGroup = findViewById<RadioGroup>(R.id.radioGroup)
+        radioGroup.setOnCheckedChangeListener { _, checkedId ->
+            replace(when (checkedId) {
+                R.id.n25 -> R.layout.fragment_main25
+                R.id.o27 -> R.layout.fragment_main27
+                else -> throw Exception("oops")
+            })
         }
-        a++
+    }
+
+    private fun replace(resId: Int) {
+        val fragment = ScrollFragment.newInstance(resId)
+        supportFragmentManager.beginTransaction()
+                .replace(R.id.container, fragment)
+                .commit()
+    }
+}
+
+class ScrollFragment : Fragment() {
+
+    companion object {
+        fun newInstance(@LayoutRes resId: Int) =
+                ScrollFragment().apply {
+                    arguments = Bundle().apply {
+                        putInt("res", resId)
+                    }
+                }
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(arguments!!.getInt("res"), null)
     }
 }
