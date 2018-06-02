bc6f626d843376a84ef0521928c6c6ba6419a5ec
ogapants <ogapamyu@gmail.com>
2018-06-02 22:39:53 +0900

    add NestedScrollView for 25,27

--- app/src/main/java/com/github/ogapants/nestedscrollchecker/MainActivity.kt
+++ app/src/main/java/com/github/ogapants/nestedscrollchecker/MainActivity.kt
@@ -2,13 +2,23 @@ package com.github.ogapants.nestedscrollchecker
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
+    companion object {
+        var a = 0
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-
+        if (a % 2 == 0) {
+            Toast.makeText(this, "--25--", Toast.LENGTH_SHORT).show()
+            setContentView(R.layout.activity_main25)
+        } else {
+            Toast.makeText(this, "--27--", Toast.LENGTH_SHORT).show()
+            setContentView(R.layout.activity_main27)
+        }
+        a++
     }
 }
