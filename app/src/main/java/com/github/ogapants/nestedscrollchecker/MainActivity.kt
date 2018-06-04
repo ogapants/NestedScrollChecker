102778f684f06a9eca9b47e821d7f6030dff9b5f
ogapan <ogapamyu@gmail.com>
2018-06-04 11:55:10 +0900

    add Scrollbar switcher

--- app/src/main/java/com/github/ogapants/nestedscrollchecker/MainActivity.kt
+++ app/src/main/java/com/github/ogapants/nestedscrollchecker/MainActivity.kt
@@ -8,6 +8,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.RadioGroup
+import android.widget.Switch
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,19 +16,28 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val radioGroup = findViewById<RadioGroup>(R.id.radioGroup)
+        val switch = findViewById<Switch>(R.id.switch_scrollbar)
         radioGroup.setOnCheckedChangeListener { _, checkedId ->
-            replace(when (checkedId) {
-                R.id.l22 -> R.layout.fragment_main22
-                R.id.m23 -> R.layout.fragment_main23
-                R.id.n25 -> R.layout.fragment_main25
-                R.id.o27 -> R.layout.fragment_main27
-                R.id.origin -> R.layout.fragment_main
-                else -> throw Exception("oops")
-            })
+            replaceFragmentWith(checkedId, switch.isChecked)
         }
+        switch.setOnCheckedChangeListener { _, isChecked ->
+            replaceFragmentWith(radioGroup.checkedRadioButtonId, isChecked)
+        }
+        radioGroup.check(R.id.origin)
     }
 
-    private fun replace(resId: Int) {
+    private fun replaceFragmentWith(checkedId: Int, checked: Boolean) {
+        replaceFragment(when (checkedId) {
+            R.id.l22 -> if (checked) R.layout.fragment_main22 else R.layout.fragment_main22_non_bar
+            R.id.m23 -> if (checked) R.layout.fragment_main23 else R.layout.fragment_main23_non_bar
+            R.id.n25 -> if (checked) R.layout.fragment_main25 else R.layout.fragment_main25_non_bar
+            R.id.o27 -> if (checked) R.layout.fragment_main27 else R.layout.fragment_main27_non_bar
+            R.id.origin -> if (checked) R.layout.fragment_main else R.layout.fragment_main_non_bar
+            else -> throw Exception("oops")
+        })
+    }
+
+    private fun replaceFragment(resId: Int) {
         val fragment = ScrollFragment.newInstance(resId)
         supportFragmentManager.beginTransaction()
                 .replace(R.id.container, fragment)
